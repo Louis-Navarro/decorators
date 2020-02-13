db18ae320c45d25fb9c5b7c61fb100152b6c0b55
Louis Navarro <azuiops.tv@gmail.com>
2020-02-13 18:12:21 +0100

    Gets version from __init__.py file

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import setuptools
+from decorators.__init__ import __version__ as v
 
 with open('README.md') as fp:
     long_description = fp.read()
 
 setuptools.setup(
     name='decorators-LOUIS-NAVARRO',
-    version="0.01a1",
+    version=v,
     author='Louis Navarro',
     description='Function decorators I made',
     long_description=long_description,
